eb20dae834dc4600a009d2a10678cf979f2d4764
Paulius <pauliusval99@hotmail.com>
2018-03-18 11:08:54 +0200

    Add project files.

---
+++ backtrack1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace backtrack1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
