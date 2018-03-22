280b80e2b5531fb348588f124c649046d7b7437b
Paulius <pauliusval99@hotmail.com>
2018-03-22 21:13:24 +0200

    viskas

---
+++ backtrack1/Maze.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace backtrack1
+{
+    public enum MoveStatus { Free, Wall, Thread, Error, Terminal };
+    public class Maze
+    {
+        private class Position
+        {
+            public int x, y;
+            public Position(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+
+            public static Position operator +(Position a, Position b)
+            {
+                return new Position(a.x + b.x, a.y + b.y);
+            }
+
+            public static Position operator -(Position a, Position b)
+            {
+                return new Position(a.x - b.x, a.y - b.y);
+            }
+        }
+
+        private Stack<int> _appliedMoves = new Stack<int>();
+
+        private int[,] _db;
+        private Position[] _moves =
+        {
+            new Position(-1, 0),
+            new Position(0, -1),
+            new Position(1, 0),
+            new Position(0, 1),
+        };
+        private Position _currentPos;
+        public int CurrentStep { get; private set; }
+        public int MoveCount
+        {
+            get
+            {
+                return _moves.Length;
+            }
+        }
+
+        public Maze(int[,] db)
+        {
+            _db = db;
+            for(int i=0; i<_db.GetLength(0); i++)
+            {
+                for(int j = 0; j<_db.GetLength(1); j++)
+                {
+                    if (_db[i, j] == 2)
+                        _currentPos = new Position(j, i);
+                }
+            }
+            CurrentStep = 2;
+        }
+
+        public int GetCurrentX()
+        {
+            return _currentPos.x + 1;
+        }
+
+        public int GetCurrentY()
+        {
+            return _currentPos.y + 1;
+        }
+        
+        public MoveStatus ApplyMove(int i, out int x, out int y)
+        {
+            x = 0;
+            y = 0;
+            if (i < 0 || i >= _moves.Length)
+                return MoveStatus.Error;
+
+            Position newPos = _currentPos + _moves[i];
+
+            x = newPos.x + 1;
+            y = newPos.y + 1;
+
+            if ((newPos.x == 0 || newPos.x == _db.GetLength(0) || newPos.y == 0 || newPos.y == _db.GetLength(1)) && _db[newPos.y, newPos.x] == 0)
+            {
+                ApplyMove(i, newPos);
+                return MoveStatus.Terminal;
+            }
+
+            if (_db[newPos.y, newPos.x] == 1)
+                return MoveStatus.Wall;
+
+            if (_db[newPos.y, newPos.x] != 0)
+                return MoveStatus.Thread;
+
+            ApplyMove(i, newPos);
+            return MoveStatus.Free;
+        }
+
+        private void ApplyMove(int i, Position newPos)
+        {
+            _appliedMoves.Push(i);
+            _currentPos = newPos;
+            _db[newPos.y, newPos.x] = ++CurrentStep;
+        }
+
+        public void UndoMove()
+        {
+            _db[_currentPos.y, _currentPos.x] = -1;
+            _currentPos -= _moves[_appliedMoves.Pop()];
+            //_appliedMoves.Pop();
+            CurrentStep--;
+        }
+        
+        public void Print(WriteAction wr)
+        {
+            wr("   ^ Y, V\n");
+            StringBuilder sb = new StringBuilder();
+            List<object> args = new List<object>();
+            int width = _db.GetLength(1);
+            for (int i = _db.GetLength(0) - 1; i >= 0; i--)
+            {
+                sb.AppendFormat("{0,3}|", i + 1);
+                for (int j = 0; j < width; j++)
+                {
+                    sb.Append("{" + j + ",5}");
+                    args.Add(_db[i, j]);
+                }
+                sb.Append("\n");
+                wr(sb.ToString(), args.ToArray());
+                sb.Clear();
+                args.Clear();
+            }
+            sb.EnsureCapacity(width * 5 + 3);
+            sb.Append("   ");
+            for (int i = 0; i < width * 5 + 3; i++)
+            {
+                sb.Append("-");
+            }
+            sb.Append("> X, U\n    ");
+            for(int i=0; i<width; i++)
+            {
+                sb.Append("    " + (i + 1).ToString());
+            }
+            sb.Append("\n");
+            wr(sb.ToString());
+        }
+    }
+}
--- backtrack1/Program.cs
+++ backtrack1/Program.cs
@@ -1,12 +1,133 @@
-﻿using System;
+﻿//#define _DEBUG
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
 
 namespace backtrack1
 {
+    public delegate void WriteAction(string format, params object[] arg);
     class Program
     {
+        private static StreamWriter _writer;
+        private static Maze _maze;
+
+        public static void Write(string format, params object[] arg)
+        {
+            #if _DEBUG
+                Console.Write(format, arg);
+            #else
+                _writer.Write(format, arg);
+            #endif
+        }
+        
+        public static int[,] ReadFile(string file)
+        {
+            int[,] res;
+            using (StreamReader reader = new StreamReader(file))
+            {
+                string[] words = reader.ReadToEnd().Split('\n');
+                int[] size = (from w in words[0].Split(' ') select Int32.Parse(w)).ToArray();
+                res = new int[size[1],size[0]];
+                for (int i = 0; i < size[1]; i++)
+                {
+                    for (int j = 0; j < size[0]; j++)
+                    {
+                        //string a = words[i + 2][j].ToString();
+                        res[size[1] - i - 1, j] = Int32.Parse(words[i + 2][j].ToString());
+                    }
+                }
+            }
+            return res;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string fin, fout;
+            try
+            {
+                fin = args[0];
+                fout = args[1];
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Incorrect arguments.\n1. input file name\n2. output file name");
+                return;
+            }
+            _writer = new StreamWriter(fout);
+            _maze = new Maze(ReadFile(fin));
+            
+            Write("PART 1. Data\n  1.1. Labyrinth\n\n");
+            _maze.Print(Write);
+            Write("\n  1.2. Initial position X={0}, Y={1}, L={2}\n", _maze.GetCurrentX(), _maze.GetCurrentY(), _maze.CurrentStep);
+
+            Write("\nPART 2. Trace\n");
+            /*
+            for (int i = 0; i < 1000000; i++)
+                Write(i.ToString() + "\n");
+            */
+            Backtrack1();
+
+            _maze.Print(Write);
+
+            _writer.Dispose();
+        }
+
+        private static int step = 1;
+
+        static bool Backtrack1()
+        {
+            StringBuilder sb = new StringBuilder();
+            int u, v;
+            int mStep = _maze.CurrentStep;
+            for(int i=0; i<_maze.MoveCount; i++)
+            {
+                Write("{0,5}) ", step++);
+                for (int j = 2; j < mStep; j++)
+                {
+                    sb.Append("-");
+                }
+                MoveStatus ms = _maze.ApplyMove(i, out u, out v);
+                sb.AppendFormat("R{0}. U={1}, V={2}. ", i + 1, u, v);
+                switch(ms)
+                {
+                    case MoveStatus.Thread:
+                        sb.Append("Thread.\n");
+                        Write(sb.ToString());
+                        break;
+                    case MoveStatus.Wall:
+                        sb.Append("Wall.\n");
+                        Write(sb.ToString());
+                        break;
+                    case MoveStatus.Free:
+                        sb.AppendFormat("Free. L:=L+1={0}. LAB[{1},{2}]:={3}.\n", _maze.CurrentStep, u, v, _maze.CurrentStep);
+                        Write(sb.ToString());
+                        if (Backtrack1())
+                            return true;
+                        break;
+                    case MoveStatus.Terminal:
+                        sb.AppendFormat("Free. L:=L+1={0}. LAB[{1},{2}]:={3}. Terminal.\n", _maze.CurrentStep, u, v, _maze.CurrentStep);
+                        Write(sb.ToString());
+                        return true;
+                    default:
+                        Write("unknown error.\n");
+                        return true;
+                }
+                sb.Clear();
+            }
+            u = _maze.GetCurrentX();
+            v = _maze.GetCurrentY();
+            _maze.UndoMove();
+            sb.Append("       ");
+            for (int i = 2; i < mStep; i++)
+            {
+                sb.Append("-");
+            }
+            sb.AppendFormat("Backtrack from X={0}, Y={1}, L={2}. LAB[{0},{1}]:=-1. L:=L-1={3}.\n", u, v, _maze.CurrentStep + 1, _maze.CurrentStep);
+            Write(sb.ToString());
+            sb.Clear();
+            return false;
         }
     }
 }
